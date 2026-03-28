9108209e765c0fba47fa8de96c30f6d924cf77ce
JUanM776 <Juan.cordobaflore@campusucc.edu.co>
2026-03-28 14:18:04 -0500

    responsive: avatar mas pequeño en mobile

--- components/AvatarGuide.tsx
+++ components/AvatarGuide.tsx
@@ -212,7 +212,7 @@ export default function AvatarGuide() {
   useEffect(() => { startFloat(); }, [startFloat]);
 
   return (
-    <div className="fixed bottom-6 right-6 z-50 flex flex-col items-end gap-3">
+    <div className="fixed bottom-4 right-4 sm:bottom-6 sm:right-6 z-50 flex flex-col items-end gap-2 sm:gap-3 scale-75 sm:scale-100 origin-bottom-right">
       <AnimatePresence mode="wait">
         <motion.div
           key={msgKey}
